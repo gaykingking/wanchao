98357c33b39e1ba22bf3850a762878c64048917f
lingsheng <1012616789@qq.com>
2020-07-28 16:13:27 +0800

    玩潮商城

---
+++ wanchao-market/src/test/java/com/lisheng/WanchaoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanchaoMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
