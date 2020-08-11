5f57f37fd30d97f07c1cf3169944e08f59cb8dc5
lingsheng <1012616789@qq.com>
2020-08-11 11:23:15 +0800

    登录

---
+++ wanchao-gateway/src/main/java/com/shengli/WanchaoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shengli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanchaoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanchaoGatewayApplication.class, args);
+    }
+
+}
---
+++ wanchao-gateway/src/test/java/com/shengli/WanchaoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shengli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanchaoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
